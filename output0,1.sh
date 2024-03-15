ec8db08811512255168e9232b127d23b131578e5
PATEL VAMSHI DHAR REDDY <49507145+patelvamshi@users.noreply.github.com>
2024-03-16 03:08:32 +0530

    change

--- output0,1.sh
+++ output0,1.sh
@@ -1,61 +1,61 @@
 echo installing node
  dnf module disable nodejs -y 
- dnf module enable nodejs:20 -y >>/tmp/expense.log
+ dnf module enable nodejs:20 -y &>>/tmp/expense.log
 echo $?
 
 echo disablling node
- dnf install nodejs -y
+ dnf install nodejs -y&>>/tmp/expense.log
 echo $?
 
 echo copying service file
- cp backend.service /etc/systemd/system/backend.service
+ cp backend.service /etc/systemd/system/backend.service&>>/tmp/expense.log
 echo $?
 
 echo adding expense
- useradd expense
+ useradd expense&>>/tmp/expense.log
 echo $?
 
 echo removing app
- rm-rf /app
+ rm-rf /app&>>/tmp/expense.log
 echo $?
 
 echo creating dir
- mkdir /app 
+ mkdir /app &>>/tmp/expense.log
 echo $?
 
 echo downlaoding 
- curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip 
+ curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
 echo $?
 
  echo app crearting files
-  cd /app 
+  cd /app &>>/tmp/expense.log
 echo $?
 
  echo unziping files
- unzip /tmp/backend.zip
+ unzip /tmp/backend.zip&>>/tmp/expense.log
 echo $?
 
 echo app
- cd /app 
- npm install 
+ cd /app &>>/tmp/expense.log
+ npm install &>>/tmp/expense.log
 echo $?
 
  echo reloading
-  systemctl daemon-reload
+  systemctl daemon-reload&>>/tmp/expense.log
 echo $?
 
  echo starting
- systemctl enable backend 
- systemctl start backend
+ systemctl enable backend &>>/tmp/expense.log
+ systemctl start backend&>>/tmp/expense.log
 echo $?
 
- echo installing sql
- dnf install mysql -y 
+ echo installing sql&>>/tmp/expense.log
+ dnf install mysql -y &>>/tmp/expense.log
 
 echo $?
 
  echo password  
- mysql -h 172.31.26.167 -uroot -pExpenseApp@1 < /app/schema/backend.sql 
+ mysql -h 172.31.26.167 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>/tmp/expense.log
 echo $?
 
 
