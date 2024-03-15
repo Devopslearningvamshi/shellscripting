0d2c4e6aff98ab703ca3f770eb7505a78225b6c0
PATEL VAMSHI DHAR REDDY <49507145+patelvamshi@users.noreply.github.com>
2024-03-16 02:15:15 +0530

    variables

---
+++ variable.sh
@@ -0,0 +1,10 @@
+ # variable with the plain text
+  c=10 
+
+  # variable with the special charc other than number, alphate
+    a="P@$$word"  
+    b="patel vamshidhar reddy" # here space also has special char so used doublecourts
+
+echo $a
+echo $b
+echo $c
